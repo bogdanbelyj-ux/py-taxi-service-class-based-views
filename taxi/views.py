250a2b4e48778e0d60fcdc638b8cef52976bf14c
Bohdan <bogdanbelyj@gmail.com>
2026-07-04 20:39:42 -0600

    Solution3

--- taxi/views.py
+++ taxi/views.py
@@ -15,6 +15,7 @@ def index(request):
 
     return render(request, "taxi/index.html", context=context)
 
+
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
     queryset = Manufacturer.objects.all()
